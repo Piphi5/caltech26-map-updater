8ce5758c244c6684c0c7c3e1c437377c3e6d1b49
Matteo Kimura <mateus.sakata@gmail.com>
2022-01-13 18:25:11 -0600

    Add storage for location data

--- get_data.py
+++ get_data.py
@@ -9,14 +9,22 @@ import requests
 from geopy import GoogleV3
 
 
-def get_latlon(geolocator, place):
-    location = geolocator.geocode(place)
-    return location.latitude, location.longitude
+def get_latlon(geolocator, location_dict, place):
+    if place in location_dict:
+        return location_dict[place]
+    else:
+        location = geolocator.geocode(place)
+        location_dict[place] = (location.latitude, location.longitude)
+        return location.latitude, location.longitude
 
 
 # Get private strings
 data_url = sys.argv[1]
 api_key = sys.argv[2]
+
+locations_df = pd.read_csv("locations.csv")
+locations_tuple = list(locations_df.to_records(index=False))
+location_dict = {place: (lat, lon) for place, lat, lon in locations_tuple}
 
 # Get data and remove personal info
 urlData = requests.get(data_url).content
@@ -25,7 +33,7 @@ df = df.drop([col for col in df.columns if col != "Hometown"], axis=1)
 
 # Setup location name to latitude longitude functions
 geolocator = GoogleV3(api_key=api_key)
-vec_address_converter = np.vectorize(partial(get_latlon, geolocator))
+vec_address_converter = np.vectorize(partial(get_latlon, geolocator, location_dict))
 lat, lon = vec_address_converter(df["Hometown"].to_numpy())
 
 # Create Map
@@ -40,3 +48,9 @@ m.fit_bounds(m.get_bounds())
 
 # Save map to html
 m.save("caltech26-map/index.html")
+
+# Save downloaded locations to a CSV
+updated_data = [(key, coords[0], coords[1]) for key, coords in location_dict.items()]
+pd.DataFrame(updated_data, columns=["Hometown", "Latitude", "Longitude"]).to_csv(
+    "locations.csv", index=False
+)
