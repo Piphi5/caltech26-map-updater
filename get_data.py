2f4a2a6b47a3529208b87df190ba217d95296d8a
Matteo Kimura <mateus.sakata@gmail.com>
2022-01-14 11:51:03 -0600

    Add clustering and data points

--- get_data.py
+++ get_data.py
@@ -6,6 +6,7 @@ import folium
 import numpy as np
 import pandas as pd
 import requests
+from folium.plugins import MarkerCluster
 from geopy import GoogleV3
 
 
@@ -16,6 +17,29 @@ def get_latlon(geolocator, location_dict, place):
         location = geolocator.geocode(place)
         location_dict[place] = (location.latitude, location.longitude)
         return location.latitude, location.longitude
+
+
+def format_info(info):
+    if pd.isna(info):
+        return ""
+    else:
+        return info
+
+
+def generate_popup(name, insta, snap, discord, other):
+    return folium.Popup(
+        html=f"""<b>Name:</b><br>
+                                {format_info(name)}<br><br>
+                                <b>Instagram:</b><br>
+                                {format_info(insta)}<br><br>
+                                <b>Snapchat:</b><br>
+                                {format_info(snap)}<br><br>
+                                <b>Discord:</b><br>
+                                {format_info(discord)}<br><br>
+                                <b>Other:</b><br>
+                                {format_info(other)}<br><br>""",
+        max_width=300,
+    )
 
 
 # Get private strings
@@ -30,7 +54,6 @@ location_dict = {place: (lat, lon) for place, lat, lon in locations_tuple}
 # Get data and remove personal info
 urlData = requests.get(data_url).content
 df = pd.read_csv(io.StringIO(urlData.decode("utf-8")))
-df = df.drop([col for col in df.columns if col != "Hometown"], axis=1)
 
 # Raise Exception if charged price may get too large
 # 200 transactions = $1 (Max limit)
@@ -40,15 +63,34 @@ if len(set(df["Hometown"]) ^ set(location_dict.keys())) >= 200:
 # Setup location name to latitude longitude functions
 geolocator = GoogleV3(api_key=api_key)
 vec_address_converter = np.vectorize(partial(get_latlon, geolocator, location_dict))
-lat, lon = vec_address_converter(df["Hometown"].to_numpy())
+latitudes, longitudes = vec_address_converter(df["Hometown"].to_numpy())
 
 # Create Map
 m = folium.Map(zoom_start=15)
+marker_cluster = MarkerCluster().add_to(m)
 
-for coord in zip(lat, lon):
+for lat, lon, name, insta, snap, discord, commited, other in zip(
+    latitudes,
+    longitudes,
+    df["Name"],
+    df["Instagram"],
+    df["Snapchat"],
+    df["Discord"],
+    df["Commited? (Y/N)"],
+    df["Other"],
+):
+    icon = "user"
+    if type(commited) is str and commited:
+        if "Y" in commited:
+            icon = "ok-sign"
+        elif "N" in commited:
+            icon = "question-sign"
+
     folium.Marker(
-        location=[coord[0], coord[1]], icon=folium.Icon(color="orange", icon="user")
-    ).add_to(m)
+        location=[lat, lon],
+        icon=folium.Icon(color="orange", icon=icon),
+        popup=generate_popup(name, insta, snap, discord, other),
+    ).add_to(marker_cluster)
 
 m.fit_bounds(m.get_bounds())
 
